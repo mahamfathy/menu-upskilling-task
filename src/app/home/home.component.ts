f4dd6dfa6986f8020c2648ec677c2fbe54e725b7
mahamfathy <mahafathyh@gmail.com>
2024-11-08 22:03:43 +0200

    finish home structure flexbox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { AboutUsComponent } from '../about-us/about-us.component';
+import { ContactUsComponent } from '../contact-us/contact-us.component';
+import { InfoComponent } from '../info/info.component';
+import { MainPageComponent } from '../main-page/main-page.component';
+import { RecipesComponent } from '../recipes/recipes.component';
+import { ReviewsComponent } from '../reviews/reviews.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MainPageComponent,
+    InfoComponent,
+    AboutUsComponent,
+    RecipesComponent,
+    ReviewsComponent,
+    ContactUsComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
