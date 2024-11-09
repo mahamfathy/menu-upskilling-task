9637025e20592b9177d3fa24a0a575a873aaa3cb
mahamfathy <mahafathyh@gmail.com>
2024-11-10 00:54:20 +0200

    add main-page image and the ingredient

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+  styleUrl: './main-page.component.scss',
 })
 export class MainPageComponent {
-
+  imagePath: string = '../../assets/img/bread.png';
 }
