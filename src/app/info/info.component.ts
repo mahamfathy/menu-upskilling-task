d853c24f8273c0260442df2c9623d734759628f2
mahamfathy <mahafathyh@gmail.com>
2024-11-10 00:27:49 +0200

    finish info component style

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './info.component.html',
-  styleUrl: './info.component.scss'
+  styleUrl: './info.component.scss',
 })
 export class InfoComponent {
-
+  foodTitle: string = 'bread';
+  images: Array<any> = [
+    '../../assets/img/image1.png',
+    '../../assets/img/image2.png',
+    '../../assets/img/image3.png',
+    '../../assets/img/image4.png',
+  ];
 }
